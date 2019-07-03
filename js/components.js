07e60d4cda5dbce0c6c565027f9233ebc2062f04
Ben Grant <benjamin.j.grant@gmail.com>
2019-07-03 15:03:55 -0700

    Add LoginForm and LogoutForm components

---
+++ js/components.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var LoginForm_1 = require("./LoginForm");
+exports.LoginForm = LoginForm_1.default;
+var LogoutForm_1 = require("./LogoutForm");
+exports.LogoutForm = LogoutForm_1.default;
