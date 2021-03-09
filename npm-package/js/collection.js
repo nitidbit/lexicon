389e7e2f80c47374633e671b38bced9c0e4c1003
Paola Castro <paolacastropaolacastro@gmail.com>
2021-03-09 11:50:02 -0800

    Version 2.9.5

--- npm-package/js/collection.js
+++ npm-package/js/collection.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.size = exports.set = exports.has = exports.entries = exports.keys = exports.get = exports.isCollection = exports.keyPathAsString = exports.keyPathAsArray = void 0;
 const isString_1 = __importDefault(require("lodash/isString"));
 const isMap_1 = __importDefault(require("lodash/isMap"));
 const isArray_1 = __importDefault(require("lodash/isArray"));
--- npm-package/js/index.js
+++ npm-package/js/index.js
@@ -1,14 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.VERSION = exports.EditWrapper = exports.Lexicon = void 0;
 var Lexicon_1 = require("./Lexicon");
-Object.defineProperty(exports, "Lexicon", { enumerable: true, get: function () { return Lexicon_1.Lexicon; } });
+exports.Lexicon = Lexicon_1.Lexicon;
 var EditWrapper_1 = require("./EditWrapper");
-Object.defineProperty(exports, "EditWrapper", { enumerable: true, get: function () { return __importDefault(EditWrapper_1).default; } });
-exports.VERSION = '2.8.4';
+exports.EditWrapper = EditWrapper_1.default;
+exports.VERSION = '2.9.4';
 /*
   Change History - API changes since last version
 
