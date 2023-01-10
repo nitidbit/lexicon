ea25d305340b03bfbea8a041f75a9c7febe204fd
Winston <winston@nitidbit.com>
2023-01-10 11:46:05 -0800

    Remove old files

--- server/db/migrate/20211006004524_add_slack_token_to_client_apps.rb
+++
@@ -1,5 +0,0 @@
-class AddSlackTokenToClientApps < ActiveRecord::Migration[5.1]
-  def change
-    add_column 'client_apps', 'slack_workflow_url', :string
-  end
-end
--- server/db/seeds.rb
+++
@@ -1,7 +0,0 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-
-
-User.create( is_admin: true, email: 'foo@nitidbit.com', password: 'joust.galaga.pacman', # Be sure to change this)
-
-
