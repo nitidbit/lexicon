0cddcd4cebf6112dc177586ba373dd0b6880e2d0
Winston <winston@nitidbit.com>
2024-07-11 17:25:37 -0700

    Make render.com build more similar to developer build

--- server/render_build.sh
+++ server/render_build.sh
@@ -1,17 +1,14 @@
 #! /usr/bin/env bash
 # executed by render.com: Settings > Build Command
 
-echo "=== render_build./.sh: building Lexicon NPM package first ==="
+echo "=== render_build./.sh: building Lexicon NPM package first: typescript -> JS ==="
 cd ../npm-package
 npm i
 npm run build
 
-echo "=== render_build./.sh: installing yarn ==="
-cd ../server
-npm i -g yarn
-
 echo "=== render_build./.sh: yarn install ==="
-yarn
+cd ../server
+npm i
 
 echo "=== render_build./.sh: bundle install ==="
 bundle install
