180708f41a497265a0d9bdb62638c30a2ac2b055
Winston <winston@nitidbit.com>
2024-07-24 10:06:27 -0700

    Clarify descriptions in build script

--- server/render_build.sh
+++ server/render_build.sh
@@ -1,17 +1,17 @@
 #! /usr/bin/env bash
 # executed by render.com: Settings > Build Command
 
-echo "=== render_build./.sh: building Lexicon NPM package first: typescript -> JS ==="
+echo "====== render_build.sh: building Lexicon NPM package first: typescript -> JS ======"
 cd ../npm-package
 npm i
 npm run build
 
-echo "=== render_build./.sh: yarn install ==="
+echo "====== render_build.sh: for install server NPM packages ======"
 cd ../server
 npm i
 
-echo "=== render_build./.sh: bundle install ==="
+echo "====== render_build.sh: server bundle install ======"
 bundle install
 
-echo "=== render_build./.sh: precompling Rails assets ==="
+echo "====== render_build.sh: precompling Rails assets ======"
 bundle exec rake assets:precompile assets:clean db:migrate
