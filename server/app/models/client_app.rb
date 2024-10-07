ebf67299228ff53ce51d1a1b32751a6f207118b0
Caroline Tan <caroline@nitidbit.com>
2024-10-07 18:31:10 -0400

    Add ransackable_associations and ransackable_attributes to fix admin page

--- server/app/models/client_app.rb
+++ server/app/models/client_app.rb
@@ -2,6 +2,26 @@
 class ClientApp < ApplicationRecord
   has_and_belongs_to_many :users
 
+  def self.ransackable_associations(_auth_object = nil)
+    ["users"]
+  end
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[
+      adapter
+      app_url
+      created_at
+      git_branch
+      github_api_token
+      github_repo
+      github_user
+      id
+      name
+      slack_workflow_url
+      updated_at
+    ]
+  end
+
   # ActiveAdmin uses this for display
   def title
     "#{name} (#{app_url})"
--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -3,6 +3,23 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :client_apps
 
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[
+      confirmation_token
+      created_at
+      email
+      encrypted_password
+      id
+      is_admin
+      remember_token
+      updated_at
+    ]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    ["client_apps"]
+  end
+
   # Allows admin > users to edit user, without changing password
   def skip_password_validation?
     true
