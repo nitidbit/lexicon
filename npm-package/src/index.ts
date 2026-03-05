56d98a2388ce0ea8bf8d378c86f7172c40b22151
chulinguy <chulinguy@gmail.com>
2026-03-05 10:41:56 -0800

    3.0.0-alpha22

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -16,7 +16,7 @@ export type LxEditPanelType = React.FC<{
   editPanelExcludeLexicons?: string[]
 }>
 
-export const VERSION = '3.0.0-alpha21'
+export const VERSION = '3.0.0-alpha22'
 
 /*
   Change History - API changes since last version
