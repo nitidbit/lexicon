e524ff44a0ec6a424f09f3f2a05079ff3287558e
chulinguy <chulinguy@gmail.com>
2026-03-23 16:08:17 -0700

    3.0.0-alpha26

--- npm-package/src/index.ts
+++ npm-package/src/index.ts
@@ -23,7 +23,7 @@ export type LxEditPanelProps = {
 
 export type LxEditPanelType = React.FC<LxEditPanelProps>
 
-export const VERSION = '3.0.0-alpha25'
+export const VERSION = '3.0.0-alpha26'
 
 /*
   Change History - API changes since last version
